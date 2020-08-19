337126bffd2b6a720cf39149a9d1e64d76724a8c
marinadoucerain <marinadoucerain@gmail.com>
2020-08-19 14:03:59 -0400

    Initial commit of all files

---
+++ PSY7105_InitiationARExercice.R
@@ -0,0 +1,26 @@
+####################################
+#         Initiation a R           #
+#           Exercice               #
+####################################
+
+# 1) Preparer un script bien formatte:
+#	- Imaginer que l'on aura besoin des modules "MASS" et "car". Charger ces modules.
+#	- Specifier le repertoire de travail ou se trouvent les donnees "datasetAnorexia.csv"
+
+# 2) Importer les données "datasetAnorexia.csv" dans un objet R appele zz. 
+# Les valeurs sont separees par des virugles (comma-delimited)
+
+# 3) Quelles sont les noms des variables?
+
+# 4) Combien d'observations y a-t'il dans ce jeu de donnees?
+
+# 5) Calculer la moyenne et l'ecart type des variables preWeight et postWeight.
+
+# 6) Calculer la difference de poids pre-post intervention dans une nouvelle variable appelee "diffPrePost"
+
+# 7) Creer un sous-ensemble dans un objet appele zz1 ne contenant que les personnes ayant suivi 
+# un traitement CBT.
+
+# 8) Effectuer un test t pour voir s'il y a une difference significative entre le poids pre et 
+# post traitement pour ces personnes du sous ensemble zz1 (seulement CBT)
+
